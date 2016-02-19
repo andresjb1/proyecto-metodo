eee56a36bf8f05ade577110010211fed3fb0e718
dennis0795 <dennis07_09@hotmail.com>
2016-02-19 12:08:51 -0600

    Cree el login

---
+++ Profe.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+    <link rel="stylesheet" type="text/css" href="diseno/HSProfe.css" />
+</head>
+<body>
+<form id="form1" runat="server">
+    <h1>Universidad Latina</h1>
+</form>
+<form>
+    <h1>Agregar Curso</h1>
+    <div class="NewClass">
+        <p>
+            <label for="NomCurso">Nombre del curso</label>
+            <input type="text" name="nomCurso" id="nomCurso"/>
+        </p>
+        <p>
+            <label for="HCurso">Horario del curso</label>
+            <input type="text" name="HCurso" id="HCurso"/>
+        </p>
+        <p>
+            <label for="NumAula">Numero del aula</label>
+            <input type="text" name="NumAula" id="NumAula"/>
+        </p>
+    </div>
+    <p class="p-container">
+
+        <input type="submit" name="go" id="go" value="Agregar Curso"/>
+    </p>
+
+
+</form>
+</body>
+</html>
+/**
+ * Created by PhpStorm.
+ * User: PCDRivera
+ * Date: 19/02/2016
+ * Time: 12:06
+ */
