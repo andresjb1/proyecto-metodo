f93d26f321a6d6f3456a87cf527fab7f2164c50b
Andrés <and-mind@hotmail.com>
2016-02-19 12:37:11 -0600

    asdasdasda

---
+++ Estudiante.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+    <link rel="stylesheet" type="text/css" href="diseno/HSEstudiante.css" />
+</head>
+<body>
+<form id="form1" runat="server">
+    <h1>Universidad Latina</h1>
+</form>
+<form>
+    <h1>Agregar Estudiante</h1>
+    <div class="NewClass">
+        <p>
+            <label for="NomEstudiante">Nombre Estudiante</label>
+            <input type="text" name="NomEstudiante" id="NomEstudiante"/>
+        </p>
+        <p>
+            <label for="NumCarnet">Carnet</label>
+            <input type="text" name="NumCarnet" id="NumCarnet"/>
+        </p>
+        <p>
+            <label for="NomCurso">Nombre Curso</label>
+            <input type="text" name="NomCurso" id="NomCurso"/>
+        </p>
+        <p>
+            <label for="NumAula">N° Aula</label>
+            <input type="text" name="NumAula" id="NumAula"/>
+        </p>
+    </div>
+    <p class="p-container">
+
+        <input type="submit" name="go" id="go" value="Agregar Estudiante"/>
+    </p>
+
+</form>
+</body>
+</html>
--- NCurso.php
+++ NCurso.php
@@ -4,7 +4,7 @@
 <head runat="server">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <title></title>
-    <link rel="stylesheet" type="text/css" href="diseno/HSProfe.css" />
+    <link rel="stylesheet" type="text/css" href="diseno/HSNCurso.css" />
 </head>
 <body>
 <form id="form1" runat="server">
