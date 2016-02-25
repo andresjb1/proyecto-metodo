3a1190d53d9e758cf449bbcb9578adb9ecd967a2
Benja <benjamin.221094@gmail.com>
2016-02-24 23:25:18 -0600

    listo

---
+++ AccesoDatos/Materia.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Benja
+ * Date: 24/02/2016
+ * Time: 23:20
+ */
+class Materia 
---
+++ AccesoDatos/Usuario.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Benja
+ * Date: 24/02/2016
+ * Time: 23:20
+ */
