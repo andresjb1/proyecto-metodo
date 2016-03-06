7db67aefabc3df43fafc18d3f87c41171ac72499
ebaldizon <emanuel.baldizon@gmail.com>
2016-03-05 21:25:55 -0600

    Pagina Home

---
+++ Presentacion(GUI)/Home.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>Home</title>
+    <link rel="stylesheet" type="text/css" href="Styles/HSGeneral.css" />
+    <link rel="stylesheet" href="Styles/Fonts.css"/>
+    <link rel="stylesheet" href="Styles/Menu.css"/>
+</head>
+<body>
+
+<header>
+    <div class="menu_bar">
+        <a href="#" class="bt-menu"><span class="icon-list"></span>Menu</a>
+    </div>
+    <nav>
+        <ul>
+            <li><a href="#"><span class="icon-house"></span> Inicio</a></li>
+            <li><a href="#"><span class="icon-suitcase"></span> Prestamos</a></li>
+            <li><a href="#"><span class="icon-rocket"></span> Registros</a></li>
+            <li><a href="#"><span class="icon-suitcase"></span> Información</a></li>
+            <li><a href="#"><span class="icon-mail"></span> Contactenos</a></li>
+        </ul>
+    </nav>
+</header>
+
+
+    <script src="http://code.jquery.com/jquery-latest.js"></script>
+    <script src="JS/menuScript.js"></script>
+</body>
+</html>
