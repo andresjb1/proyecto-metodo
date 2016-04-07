f3137068295dbf381e6dd15eda38fa1b7fe210f2
Benja <benjamin.221094@gmail.com>
2016-04-06 19:20:22 -0600

    listo

---
+++ Presentacion(GUI)/Styles/Perfil.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>Home</title>
+    <link rel="shortcut icon" href="IMG/favicon.png"/>
+    <link rel="stylesheet" type="text/css" href="Styles/HSGeneral.css" />
+    <link rel="stylesheet" href="Styles/Fonts.css"/>
+    <link rel="stylesheet" href="Styles/Menu.css"/>
+</head>
+<body>
+
+<header>
+    <div class="menu_bar">
+
+        <a href="#" class="bt-menu"><img id="logo_menu" src="IMG/logo-latina-2015.png"/>
+            <span class="icon-list"></span>
+        </a>
+    </div>
+    <nav>
+        <ul>
+            <img id="logo_menu_max" src="IMG/logo-latina-2015.png"/>
+            <li><a href="#"><span class="icon-home"></span> Inicio</a></li>
+            <li><a href="Dispositivo.php"><span class="icon-add-to-list"></span> Solicitar Dispositivo</a></li>
+            <li><a href="#"><span class="icon-list"></span> Dispositivos Disponibles</a></li>
+            <li><a href="../Negocio/Cierre.php"><span class="icon-tag"></span> Cerrar sesión</a></li>
+        </ul>
+    </nav>
+</header>
+
+
+<script src="http://code.jquery.com/jquery-latest.js"></script>
+<script src="JS/menuScript.js"></script>
+</body>
+</html>
